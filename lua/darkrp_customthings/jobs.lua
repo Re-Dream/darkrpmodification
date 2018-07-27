4654290942c12592ec21d3d610bb62b3c0cf5497
Etothepowerof26 <refertrionjared1@gmail.com>
2018-07-27 18:47:08 -0400

    Hitman job. Needs testing

--- lua/darkrp_customthings/jobs.lua
+++ lua/darkrp_customthings/jobs.lua
@@ -16,7 +16,30 @@ http://wiki.darkrp.com/index.php/DarkRP:CustomJobFields
 Add your custom jobs under the following line:
 ---------------------------------------------------------------------------]]
 
-
+TEAM_HITMAN = DarkRP.createJob("Hitman", {
+	color = Color(255, 140, 0, 255),
+	model = "models/player/leet.mdl",
+	description = [[You take hits from people for money.]],
+	weapons = {},
+	command = "hitmanjob",
+	max = 0, -- at most 70% of the players can have this job. Set to a whole number to set an absolute limit.
+	salary = 45,
+	admin = 0,
+	vote = false,
+	hasLicense = false,
+	-- The following fields are OPTIONAL. If you do not need them, or do not need to change them from their defaults, REMOVE them.
+	modelScale = 1,
+	maxpocket = 20,
+	maps = {"rp_downtown_v2", "gm_construct"},
+	ammo = {
+		["pistol"] = 60,
+	},
+    category = "Other", -- The name of the category it is in. Note: the category must be created!
+    sortOrder = 100, -- The position of this thing in its category. Lower number means higher up.
+ 
+    playerClass = "player_darkrp",
+    label = "Hitman", -- Optional: the text on the button in the F4 menu
+})
 
 --[[---------------------------------------------------------------------------
 Define which team joining players spawn into and what team you change to if demoted
@@ -35,3 +58,4 @@ GAMEMODE.CivilProtection = {
 Jobs that are hitmen (enables the hitman menu)
 ---------------------------------------------------------------------------]]
 DarkRP.addHitmanTeam(TEAM_MOB)
+DarkRP.addHitmanTeam(TEAM_HITMAN)
